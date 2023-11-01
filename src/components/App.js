b2720009f9b0b77c96c6a48e87f2f0aebde97a6a
George <georgejoj28@gmail.com>
2023-11-01 22:28:21 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+
+
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskList, setTaskList] = useState(TASKS)
+
+  function handleDelete(text) {
+    // console.log(id)
+    const deleteItem = taskList.filter(task => task.text !== text)
+    setTaskList(deleteItem)
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList handleDelete={handleDelete}  tasks={taskList}/>
     </div>
   );
 }
